458f155bd1abc331c5ee34b25f81bba40e017af2
prasenjit <prasenjuetce@gmail.com>
2020-08-19 21:32:41 +0530

    od demo checkin

---
+++ object_detection/main.py
@@ -0,0 +1,175 @@
+### https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/auto_examples/plot_object_detection_checkpoint.html ###
+
+import os
+import sys
+import pathlib
+import tensorflow as tf
+import time
+
+# Add paths to make library happy
+sys.path.append(os.path.join(os.getcwd(), 'models'))
+sys.path.append(os.path.join(os.getcwd(), 'models\\research'))
+
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+
+# Make sure to comment out #import matplotlib; matplotlib.use('Agg') in visualization_utils if you want to see the output images in UI
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
+tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+def download_images():
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/test_images/'
+    filenames = ['image1.jpg', 'image2.jpg']
+    image_paths = []
+    for filename in filenames:
+        image_path = tf.keras.utils.get_file(fname=filename,
+                                            origin=base_url + filename,
+                                            untar=False)
+        image_path = pathlib.Path(image_path)
+        image_paths.append(str(image_path))
+    return image_paths
+
+IMAGE_PATHS = download_images()
+
+# Download and extract model
+def download_model(model_name, model_date):
+    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + model_date + '/' + model_file,
+                                        untar=True)
+    return str(model_dir)
+
+MODEL_DATE = '20200711'
+MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
+#'faster_rcnn_resnet152_v1_1024x1024_coco17_tpu-8'
+#'ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8'
+#'centernet_hg104_1024x1024_coco17_tpu-32'
+
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
+
+# Download labels file
+def download_labels(filename):
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
+    label_dir = tf.keras.utils.get_file(fname=filename,
+                                        origin=base_url + filename,
+                                        untar=False)
+    label_dir = pathlib.Path(label_dir)
+    return str(label_dir)
+
+LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
+PATH_TO_CFG = PATH_TO_MODEL_DIR + "/pipeline.config"
+PATH_TO_CKPT = PATH_TO_MODEL_DIR + "/checkpoint"
+
+print('Loading model... ', end='')
+start_time = time.time()
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
+model_config = configs['model']
+detection_model = model_builder.build(model_config=model_config, is_training=False)
+
+# Restore checkpoint
+ckpt = tf.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(PATH_TO_CKPT, 'ckpt-0')).expect_partial()
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+@tf.function
+def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+
+    return detections
+    
+def load_image_into_numpy_array(path):
+    """Load an image from file into a numpy array.
+
+    Puts image into numpy array to feed into tensorflow graph.
+    Note that by convention we put it into a numpy array with shape
+    (height, width, channels), where channels=3 for RGB.
+
+    Args:
+      path: the file path to the image
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3)
+    """
+    return np.array(Image.open(path))
+
+
+for image_path in IMAGE_PATHS:
+
+    print('Running inference for {}... '.format(image_path), end='')
+    start_time = time.time()
+
+    image_np = load_image_into_numpy_array(image_path)
+
+    # Things to try:
+    # Flip horizontally
+    # image_np = np.fliplr(image_np).copy()
+
+    # Convert image to grayscale
+    # image_np = np.tile(
+    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+
+    detections = detect_fn(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=200,
+            min_score_thresh=.30,
+            agnostic_mode=False)
+
+    plt.figure()
+    plt.imshow(image_np_with_detections)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('Done! Took {} seconds'.format(elapsed_time))
+plt.show()
